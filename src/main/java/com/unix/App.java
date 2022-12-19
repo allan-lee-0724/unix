0a8cb4ae51f6e4b81f8ce2c9aff881eddcdf2893
allan-lee-0724 <allan841@revature.net>
2022-12-19 10:22:35 -0500

    first commit

---
+++ src/main/java/com/unix/App.java
@@ -0,0 +1,19 @@
+package com.unix;
+
+import io.javalin.Javalin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Javalin app = Javalin.create();
+
+        app.get("/hello", ctx -> ctx.result("Hello world!"));
+
+        app.start();
+    }
+}
